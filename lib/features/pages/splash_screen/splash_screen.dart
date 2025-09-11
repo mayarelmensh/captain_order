c0c8ca7a151821ebc54b25d90e99fdbf6da39c76
Zaghloul <ahmedalighazy824@gmail.com>
2025-09-11 13:05:54 +0300

    Fixed ui .

---
+++ lib/features/pages/splash_screen/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../../../controller/cache/shared_preferences_utils.dart';
+import '../../../core/utils/app_routes.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
+  _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (!mounted) return;
+
+    bool? isFirstTime = SharedPreferenceUtils.getData(key: 'isFirstTime') as bool?;
+    String? token = SharedPreferenceUtils.getData(key: 'token') as String?;
+
+    if (isFirstTime == false && token != null) {
+      Navigator.pushReplacementNamed(context, AppRoutes.homeRoute);
+    } else if (isFirstTime == false) {
+      Navigator.pushReplacementNamed(context, AppRoutes.loginRoute);
+    } else {
+      Navigator.pushReplacementNamed(context, AppRoutes.onBoardingRoute);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/splash_screen.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -5,7 +5,7 @@ import 'controller/cache/shared_preferences_utils.dart';
 import 'core/utils/app_routes.dart';
 import 'features/auth/login/view/login_screen.dart';
 import 'features/first_screen/on_boadrding/on_boarding_screen.dart';
-import 'features/first_screen/splash_screen.dart';
+import 'features/pages/splash_screen/splash_screen.dart';
 
 Future<void> mainCommon(AppConfig config) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,19 +26,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize: const Size(375, 812), // مقاس التصميم من Figma مثلاً
       minTextAdapt: true,
       splitScreenMode: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        title: config.appName,
         initialRoute: AppRoutes.splashRoute,
         routes: {
           AppRoutes.splashRoute: (context) => const SplashScreen(),
           AppRoutes.onBoardingRoute: (context) => const OnBoardingScreen(),
           AppRoutes.loginRoute: (context) => const LoginScreen(),
           // TODO: ضيفي باقي الروتس هنا زي AppRoutes.homeRoute
+          // AppRoutes.homeRoute: (context) => const HomeScreen(),
         },
-        title: config.appName,
       ),
     );
   }
